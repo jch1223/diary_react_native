338ce755ea0db2e93b253ca43fc5d536c70d6ee3
jch1223 <ico1828@gmail.com>
2020-02-10 01:43:45 +0900

    탭 화면 구성

--- Navigator.js
+++ Navigator.js
@@ -6,23 +6,36 @@ import { Ionicons } from "@expo/vector-icons";
 import ListScreen from "./screens/ListScreen";
 import BookmarkScreen from "./screens/BookmarkScreen";
 
-const TabNavigator = createBottomTabNavigator({
-  List: {
-    screen: ListScreen,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <Ionicons name='md-list-box' size={25} color={tintColor} />;
+const TabNavigator = createBottomTabNavigator(
+  {
+    List: {
+      screen: ListScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => {
+          return <Ionicons name='md-list-box' size={25} color={tintColor} />;
+        },
+        tabBarLabel: "목록"
+      }
+    },
+    Bookmark: {
+      screen: BookmarkScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => {
+          return <Ionicons name='md-heart' size={25} color={tintColor} />;
+        },
+        tabBarLabel: "즐겨찾기"
       }
     }
   },
-  Bookmark: {
-    screen: BookmarkScreen,
-    navigationOptions: {
-      tabBarIcon: ({ tintColor }) => {
-        return <Ionicons name='md-heart' size={25} color={tintColor} />;
-      }
+  {
+    tabBarOptions: {
+      activeTintColor: "#424242",
+      inactiveTintcolor: "#9e9e9e",
+      borderTopColor: "#bdbdbd",
+      backgroundColor: "#ffffff"
+      // showLabel: false  //탭의 글씨를 삭제함
     }
   }
-});
+);
 
 export default createAppContainer(TabNavigator);
--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-navigation";
 
 const ListScreen = () => {
-  return <Text>목록 화면</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>목록 화면</Text>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default ListScreen;
