8fcdf135f7fc55903c21c5e462da7ffbc90e5c9a
jch1223 <ico1828@gmail.com>
2020-02-15 21:28:31 +0900

    글 읽기 화면 / 본문

--- screens/ViewScreen.js
+++ screens/ViewScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, ScrollView, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import ViewHeader from "../components/ViewHeader";
 
@@ -7,6 +7,10 @@ function ViewScreen(props) {
   return (
     <SafeAreaView style={styles.container}>
       <ViewHeader />
+      <ScrollView>
+        <Text style={styles.content}>글 내용</Text>
+        <Text style={styles.date}>2020년 02월 15일</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -14,6 +18,18 @@ function ViewScreen(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  content: {
+    padding: 20,
+    fontSize: 16,
+    lineHeight: 20,
+    color: "#424242"
+  },
+  date: {
+    padding: 20,
+    paddingTop: 0,
+    fontSize: 12,
+    color: "#bdbdbd"
   }
 });
 
