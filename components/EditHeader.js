0dece50f14d0d76e0448c88bb8e3ccf507c20308
jch1223 <ico1828@gmail.com>
2020-02-15 20:25:10 +0900

    글 작성 화면

---
+++ components/EditHeader.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { withNavigation } from "react-navigation";
+import { Ionicons } from "@expo/vector-icons";
+
+function EditHeader({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => {
+          navigation.goBack();
+        }}
+        hitSlop={{ top: 32, bottom: 32, left: 32, right: 32 }}
+      >
+        <Ionicons name='ios-arrow-back' size={28} color='#da5746' />
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.8} hitSlop={{ top: 32, bottom: 32, left: 32, right: 32 }}>
+        <Text style={styles.done}>완료</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 48,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 16,
+    paddingLeft: 16
+  },
+  done: {
+    fontSize: 16,
+    color: "#da5746"
+  }
+});
+
+export default withNavigation(EditHeader);
--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
+import EditHeader from "../components/EditHeader";
 
 const EditScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>글쓰기 화면</Text>
+      <EditHeader />
+      <View style={styles.body}>
+        <TextInput placeholder='제목' style={styles.title} />
+        <View style={styles.divider} />
+        <TextInput placeholder='이곳을 눌러 작성을 시작하세요' multiline={true} style={styles.content} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -13,6 +19,31 @@ const EditScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  body: {
+    flex: 1,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#212121"
+  },
+  content: {
+    flex: 1,
+    fontSize: 16,
+    lineHeight: 20,
+    color: "#424242"
+  },
+  divider: {
+    marginTop: 12,
+    marginBottom: 12,
+    height: 1,
+    width: "100%",
+    backgroundColor: "#dedede"
   }
 });
 
