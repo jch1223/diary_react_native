45db1f3d709e5f6d20cc32502fa79712ee7c3af0
jch1223 <ico1828@gmail.com>
2020-03-16 16:32:11 +0900

    즐겨찾기 목록 화면 추가

--- screens/BookmarkScreen.js
+++ screens/BookmarkScreen.js
@@ -2,11 +2,20 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import Header from "../components/Header";
+import ArticleItem from "../components/ArticleItem";
 
 const BookmarkScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header title='즐겨찾기' />
+      <ArticleItem
+        article={{
+          id: 1,
+          title: "첫번째 글",
+          content: "첫번째 내용",
+          date: "2020년 2월 14일"
+        }}
+      />
     </SafeAreaView>
   );
 };
